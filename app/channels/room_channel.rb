d7afa3f3dc080e099fc1c2e29930ebd7c008f43f
yasudamakoto <hare.v1v1enne@gmail.com>
2022-03-06 05:43:51 +0900

    Action Cableを用いたwebsocketを実装

---
+++ app/channels/room_channel.rb
@@ -0,0 +1,21 @@
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    stream_from "room_channnel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak(data)
+    # ActionCable.server.broadcast(
+    #   'room_channel',
+    #   {
+    #     message: data['message']
+    #   }
+    # )
+
+    Message.create! content: data['message']
+  end
+end
